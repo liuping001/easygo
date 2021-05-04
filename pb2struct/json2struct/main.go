12520694490a366e8a5ca594c2cd44de3a20de06
liuping001 <692603114@qq.com>
2021-05-04 11:02:15 +0800

    json2struct: 写文件

--- pb2struct/json2struct/main.go
+++ pb2struct/json2struct/main.go
@@ -8,10 +8,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 )
 
 var (
 	file       = flag.String("file", "", "json")
+	outDir     = flag.String("out", "./", "out dir")
 	structType = flag.String("to_type", "rapidjson", "watch cpp json lib. rapidjson")
 )
 
@@ -24,19 +28,51 @@ func ReadAll(filePath string) ([]byte, error) {
 	return ioutil.ReadAll(file)
 }
 
-func toRipadJson(data []byte) {
-	{
-		t := JsonOutStruct{StructOutI(&CppStructOut{}),
-			TypeTransformI(&CppTypeTransform{}),
-			[]string{}}
-		t.Object([]byte("root"), []byte(data))
+func FileName(file string) string {
+	base := filepath.Base(file)
+	suffix := path.Ext(base)
+	return strings.TrimSuffix(base, suffix)
+}
+
+func NewFile(file string, dir string, suffix string) (*os.File, error) {
+	fileName := fmt.Sprintf("%s/%s%s", dir, FileName(file), suffix)
+	return os.OpenFile(
+		fileName,
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0666,
+	)
+}
+
+type OutInfo struct {
+	path   string
+	outDir string
+}
+
+func toRipadJson(data []byte, out OutInfo) {
+	header := JsonOutStruct{StructOutI(&CppStructOut{}),
+		TypeTransformI(&CppTypeTransform{}),
+		[]string{}}
+	header.Object([]byte("root"), data)
+
+	body := JsonOutParseFunc{OutJsonFuncI(&RapidJsonParseFuncOut{}),
+		TypeTransformI(&CppTypeTransform{}),
+		[]string{}}
+	body.Object([]byte("root"), data)
+
+	h, err := NewFile(out.path, out.outDir, ".h")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
-	{
-		t := JsonOutParseFunc{OutJsonFuncI(&RapidJsonParseFuncOut{}),
-			TypeTransformI(&CppTypeTransform{}),
-			[]string{}}
-		t.Object([]byte("root"), []byte(data))
+	defer h.Close()
+	h.WriteString(fmt.Sprintf("namespace %s{\n", FileName(out.path)))
+	for _, item := range header.OutString {
+		h.WriteString(item)
 	}
+	for _, item := range body.OutString {
+		h.WriteString(item)
+	}
+	h.WriteString(fmt.Sprintf("\n}\n"))
 }
 
 func main() {
@@ -44,12 +80,12 @@ func main() {
 
 	json, err := ReadAll(*file)
 	if err != nil {
-		fmt.Print("err:%s", err.Error())
+		fmt.Printf("err:%s", err.Error())
 		os.Exit(1)
 		return
 	}
 	if *structType == "rapidjson" {
-		toRipadJson(json)
+		toRipadJson(json, OutInfo{*file, *outDir})
 	}
 
 }
