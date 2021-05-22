31b3dcadc83ade23cbf293a6535380869e8c6b28
liuping001 <692603114@qq.com>
2021-05-22 22:29:49 +0800

    draw: redis

--- draw/redis/show.go
+++ draw/redis/show.go
@@ -21,6 +21,17 @@ func show(w http.ResponseWriter, r *http.Request) {
 	}
 
 	cmds := strings.Fields(cmd)
+
+	if len(cmds) <= 0 {
+		fmt.Fprint(w, "cmd wrong:"+cmd)
+		return
+	}
+
+	if !checkCmd(cmds[0]) {
+		fmt.Fprint(w, "cmd not allow:"+cmd)
+		return
+	}
+
 	var args []interface{}
 	for _, value := range cmds {
 		args = append(args, value)
@@ -36,17 +47,46 @@ func show(w http.ResponseWriter, r *http.Request) {
 	case *redis.ClusterClient:
 		ret = redisClient.(*redis.ClusterClient).Do(ctx, args...)
 	}
-	if ret.Err() != nil {
+	if ret.Err() != nil && ret.Err() != redis.Nil {
 		fmt.Fprint(w, fmt.Sprintf("redis do cmd error:{%s} cmds:%v\n", ret.Err().Error(), cmds))
 		return
 	}
 
 	val := ret.Val()
 	data, err := draw.DrawSvg(&val)
-
 	if err != nil {
 		fmt.Fprint(w, fmt.Sprintf("val:%v, err:%s", val, err.Error()))
 		return
 	}
 	fmt.Fprint(w, data)
+}
+
+var allowCmd = map[string]bool{
+	"scan":          true,
+	"type":          true,
+	"ttl":           true,
+	"get":           true,
+	"mget":          true,
+	"lrange":        true,
+	"smembers":      true,
+	"hscan":         true,
+	"sscan":         true,
+	"hget":          true,
+	"hmget":         true,
+	"hgetall":       true,
+	"zrange":        true,
+	"zrevrange":     true,
+	"zrank":         true,
+	"zrevrank":      true,
+	"zscore":        true,
+	"zcard":         true,
+	"zrangebyscore": true,
+	"xrange":        true,
+	"keys":          true,
+	"info":          true,
+}
+
+func checkCmd(cmd string) bool {
+	_, ok := allowCmd[cmd]
+	return ok
 }
